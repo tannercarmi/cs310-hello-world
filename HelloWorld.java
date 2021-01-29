3d9abc515f836e43cdb05156e4f7dc347c159b3a
tannercarmi <tcarmichael1@stu.jsu.edu>
2021-01-29 16:45:43 -0600

    Removed f3()

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,8 +6,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
         System.out.println("Hello, World!");
     }
     private void f1() {
@@ -15,9 +15,6 @@ public class HelloWorld {
     }
     private void f2() {
         // v2.0
-    }
-    private void f3() {
-        // v3.0
     }
 
 }
